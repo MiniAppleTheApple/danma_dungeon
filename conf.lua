775a9e4c4b485671857e0acbe7ed4dddb22cd0bc
MiniApple <chenxiuhua19780828@gmail.com>
2023-07-28 22:00:42 -0300

    Added basic control and physics for player

--- conf.lua
+++ conf.lua
@@ -1,3 +1,5 @@
 function love.conf(t)
     t.console = true
+    t.window.width = 320
+    t.window.height = 320
 end
--- main.lua
+++ main.lua
@@ -1,28 +1,32 @@
 local wf = require('windfield')
 
+local SPEED = 200
 
 local world, player
 
 function love.load()
     world = wf.newWorld(0, 0, true)
+    player = {
+        collider = world:newRectangleCollider(0, 0, 32, 32),
+    }
 end
 
 function love.update(dt)
-    world:update(dt)
     if love.keyboard.isDown('w') then
-        player.y = player.y - SPEED * dt
+        player.collider:applyForce(0, -SPEED)
     elseif love.keyboard.isDown('s') then
-        player.y = player.y + SPEED * dt
+        player.collider:applyForce(0, SPEED)
     end
     if love.keyboard.isDown('a') then
-        player.x = player.x - SPEED * dt
+        player.collider:applyForce(-SPEED, 0)
     elseif love.keyboard.isDown('d') then
-        player.x = player.x + SPEED * dt
+        player.collider:applyForce(SPEED, 0)
     end
+    world:update(dt)
 end
 
 function love.draw()
     love.graphics.clear(0, 0, 0)
     love.graphics.setColor(255, 255, 255)
-    love.graphics.rectangle('fill', player.x, player.y, player.height, player.width)
+    world:draw()
 end
